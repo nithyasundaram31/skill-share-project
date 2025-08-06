010b4cdc35d5697aa756b39ff63367065d70a2fe
Nithya <nithyasundaram31@gmail.com>
2025-08-06 09:18:54 +0530

    cors setup

--- app.js
+++ app.js
@@ -7,10 +7,19 @@ const termRoute = require("./routers/termRoute");
 const resourceRoute = require("./routers/resourceRoute");
 const commentRoute = require("./routers/commentRoute");
 
-// const cors = require('cors');
-
 
 const app = express();
+
+app.use(cors({
+    origin: 'https://resilient-brioche-1cb6d6.netlify.app', // Replace with your frontend URL
+    credentials: true, // Allow credentials to be sent
+
+
+  //   origin: 'http://localhost:5173', // your Netlify frontend URL
+  // credentials: true, 
+
+}));
+
 //Middleware to parse JSON request bodies
 app.use(express.json());
 
@@ -21,11 +30,10 @@ app.use('/api/auth',userRoute);
    app.use('/api/comment',commentRoute);
   
 
+app.get("/", (req, res) => {
+    res.send("server is running..")
+})
 
-
-//parsing incoming JSON request
-// app.use(express.json({ limit: '50mb' }));
-// app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 
 // Middleware to handle 404 errors
