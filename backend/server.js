45d67fd9c7edddd95968427ca650fc165060bea7
Nithya <nithyasundaram31@gmail.com>
2025-08-07 21:28:25 +0530

    connect the server by mongoose

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const app = require('./app');
 const mongoose = require("mongoose");
 const { MONGODB_URI } = require("./utils/config");
 
+
 mongoose.connect(MONGODB_URI)
 .then(()=>{
     console.log("connected to the mongodb")
