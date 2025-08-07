ad7078948a980e6a47cb826f0b0dbfb611db98b8
Nithya <nithyasundaram31@gmail.com>
2025-08-07 19:36:15 +0530

    redux store

---
+++ fronted/src/redux/app/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import registerReducer from '../features/auth/registerSlice';
+import loginReducer from '../features/auth/loginSlice';
+
+const store = configureStore({
+    reducer: {
+        register: registerReducer,
+        login: loginReducer,
+       
+    },
+})
+
+export default store;
