a47e1aa5467d625fd2b21b3a4d8eba9818fb8f9d
Nithya <nithyasundaram31@gmail.com>
2025-08-09 09:29:43 +0530

    add existing term in createterm

--- backend/controllers/termController.js
+++ backend/controllers/termController.js
@@ -5,17 +5,26 @@ exports.createTerm = async (req, res) => {
   try {
     const { name } = req.body;
     const term = await Term.create({ name });
-    res.status(201).json(term);
+      const existingTerm = await User.findOne({ name });
+    
+            if (existingTerm) {
+                return res.status(400).json({ message: 'term already exists' });
+            }
+            console.log(existingTerm)
+    
+    res.status(201).json({term, message:"Term creation sucessfully"});
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 };
 
+
 // Get All Terms
 exports.getTerms = async (req, res) => {
   try {
     const terms = await Term.find();
-    res.json(terms);
+    
+    res.status(201).json({terms});
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
