bf52bb0b825840e1eb7567369e65b7b044673024
Nithya <nithyasundaram31@gmail.com>
2025-08-09 13:39:24 +0530

    term services done---

--- frontend/src/services/termServices.js
+++ frontend/src/services/termServices.js
@@ -5,8 +5,15 @@ const termServices={
 createTerm: async(userData)=>{
  return await instance.post('/term',userData)
 },
-
-
+getTerms:async()=>{
+    return await instance.get('/term')
+},
+updateTerm:async(id,userData)=>{
+   return await instance.update(`/term/${id}`,userData)
+},
+deleteTerm:async(id)=>{
+   return await instance.delete(`/term/${id}`)
+}
 
 }
 
