c59277343644987cd20b3077e0d8c3fdc92f6bcb
Nithya <nithyasundaram31@gmail.com>
2025-08-09 16:45:56 +0530

    crud servide done

--- frontend/src/services/termServices.js
+++ frontend/src/services/termServices.js
@@ -9,7 +9,7 @@ getTerms:async()=>{
     return await instance.get('/term')
 },
 updateTerm:async(id,userData)=>{
-   return await instance.update(`/term/${id}`,userData)
+   return await instance.put(`/term/${id}`,userData)
 },
 deleteTerm:async(id)=>{
    return await instance.delete(`/term/${id}`)
