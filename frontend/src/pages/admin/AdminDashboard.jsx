881f661734619b779bdb4d724fdbd873a952f48f
Nithya <nithyasundaram31@gmail.com>
2025-08-09 09:25:23 +0530

    admindashboard  fixed in outlet

--- frontend/src/pages/admin/AdminDashboard.jsx
+++ frontend/src/pages/admin/AdminDashboard.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import SideBar from '../../components/sideBar'
-import NavBar from '../../components/NavBar'
-import { Outlet } from 'react-router-dom'
-
+import { Outlet } from "react-router";
+import NavBar from "../../components/NavBar";
+import SideBar from "../../components/sideBar";
 
 function AdminDashboard() {
   return (
-    <>
-    <NavBar/>
-    <SideBar/>
-     <div className="p-4">
-          <Outlet /> {/* Show nested page here */}
+    <div className="flex">
+      <SideBar />
+      <div className="flex-1">
+        <NavBar />
+        <div className="p-4 flex justify-center  items-center  md:ml-36">
+          <Outlet /> {/* Nested routes */}
         </div>
-    </>
-  )
+      </div>
+    </div>
+  );
 }
 
 export default AdminDashboard
