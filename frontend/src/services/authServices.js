38bba87044799d2ddb85e28d74e5e5ba1594f073
Nithya <nithyasundaram31@gmail.com>
2025-08-21 18:57:25 +0530

    add a profile services

--- frontend/src/services/authServices.js
+++ frontend/src/services/authServices.js
@@ -8,14 +8,17 @@ const authServices = {
         return await instance.post('/auth/login', userData);
     },
     
-   getProfile:async()=>{
-return await instance.get('/auth/getProfile')
+   getProfile:async(id)=>{
+return await instance.get(`/auth/profile/${id}`)
 
    },
 
    updateProfile:async(id, userData)=>{
-    return await instance.put(`/auth/updateProfile/${id}`, userData)
+    return await instance.put(`/auth/profile/${id}`, userData)
+   },
+   getAllUsers:async()=>{
+    return await instance.get('/auth/all')  //its a protected route we have to send ta token through autherization hearder 
    }
-}
+}  
 
 export default authServices;
