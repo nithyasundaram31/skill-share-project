bf43a874b895a965d5ad6080a758fa1da9c3f132
Nithya <nithyasundaram31@gmail.com>
2025-08-21 18:59:00 +0530

    create userdahboard

--- frontend/src/pages/user/UserDashboardPage.jsx
+++ frontend/src/pages/user/UserDashboardPage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function UserDashboardPage() {
   return (
-    <div className='text-4xl text-yellow-600 text-center'>UserDashboardPage</div>
+    <div className='text-4xl p-6 text-yellow-600 text-center'>UserDashboardPage</div>
   )
 }
 
