c61519b3462ed50bacde81965d1a018a8e5cbafe
Nithya <nithyasundaram31@gmail.com>
2025-08-21 18:58:05 +0530

    create user dahboard

--- frontend/src/pages/user/UserDashboard.jsx
+++ frontend/src/pages/user/UserDashboard.jsx
@@ -5,15 +5,17 @@ import { Outlet } from 'react-router-dom'
 
 
 function UserDashboard() {
-  return (
-    <>
-    <NavBar/>
-    <SideBar/>
-     <div className="p-4 ">
-          <Outlet /> {/* Show nested page here */}
-        </div>
-    </>
-  )
+   return (
+     <div className="flex">
+       <SideBar />
+       <div className="flex-1">
+         <NavBar />
+         <div className="p-4  mt-14   lg:ml-36">
+           <Outlet /> {/* Nested routes */}
+         </div>
+       </div>
+     </div>
+   );
 }
 
 export default UserDashboard
