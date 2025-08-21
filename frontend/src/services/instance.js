8bac99fc88b8b0c78fa64977ed6dbd885518ae6e
Nithya <nithyasundaram31@gmail.com>
2025-08-21 18:55:57 +0530

    add automatic token intercepator

--- frontend/src/services/instance.js
+++ frontend/src/services/instance.js
@@ -12,4 +12,13 @@ const instance = axios.create({
     withCredentials: true,
 });
 
+// instance.js
+instance.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
+
 export default instance;
