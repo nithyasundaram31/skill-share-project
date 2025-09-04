b7d51034c2bc0cd9ac5c8468d0d245bb5c765102
Nithya <nithyasundaram31@gmail.com>
2025-09-04 13:38:43 +0530

    backend url added

--- frontend/src/services/instance.js
+++ frontend/src/services/instance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = (import.meta.env.VITE_API_URL || 'http://127.0.0.1:5001') + '/api';
+const baseURL = (import.meta.env.VITE_API_URL || 'https://skill-share-project-1.onrender.com/') + '/api';
 
 const instance = axios.create({
     baseURL,
